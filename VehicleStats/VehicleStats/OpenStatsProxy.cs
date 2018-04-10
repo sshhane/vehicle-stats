0c69f36b8af5c5faa4fa92e18847e19121cf6914
sshhane <g00332592@gmit.ie>
2018-04-10 12:06:02 +0100

    getStats added

---
+++ VehicleStats/VehicleStats/OpenStatsProxy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleStats
+{
+    class OpenStatsProxy
+    {
+        public static RootObject GetStats(string make, string model, string year)
+        {
+
+        }
+    }
+
+    public class Result
+    {
+        public string VehicleDescription { get; set; }
+        public int VehicleId { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int Count { get; set; }
+        public string Message { get; set; }
+        public List<Result> Results { get; set; }
+    }
+}
