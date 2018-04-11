ee8a37410bda31e971d1200c9c084e5171c6d3d0
sshhane <g00332592@gmit.ie>
2018-04-11 18:08:14 +0100

    api call for make

--- VehicleStats/CrashStats/CrashStats/Makes.cs
+++ VehicleStats/CrashStats/CrashStats/Makes.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,34 @@ namespace CrashStats
     class Makes
     {
         //public static async Task<YearRootObject> GetMakes(string y){}
+        public static int[] modelYear = new int[999];
+
+        public static async Task<YearRootObject> GetYears()
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync("https://one.nhtsa.gov/webapi/api/SafetyRatings/?format=json");
+
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(YearRootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (YearRootObject)serializer.ReadObject(ms);
+
+            // int[] modelYear = new int[data.Results.Count()];
+            Debug.WriteLine("test")
+ ;
+            // loop over, return ModelYear
+            for (int i = 0; i < data.Results.Count(); i++)
+            {
+                modelYear[i] = data.Results[i].ModelYear;
+                Debug.WriteLine("Year: " + modelYear[i]);
+            }
+
+            //modelYear = modelYear.Where(c => c != null).ToArray();
+
+            //modelYear = modelYear.Where(s => !String.IsNullOrEmpty(s)).ToArray();
+            return data;
+        }
     }
 
     [DataContract]
