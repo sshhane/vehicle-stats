76c3358e3b95dd32a364a7b8440a5812c07b2a72
sshhane <g00332592@gmit.ie>
2018-04-13 23:45:34 +0100

    cleaned up folders

--- CrashStats/CrashStats/obj/x86/Debug/XamlTypeInfo.g.cs
+++ CrashStats/CrashStats/obj/x86/Debug/XamlTypeInfo.g.cs
@@ -132,15 +132,17 @@ namespace CrashStats.CrashStats_XamlTypeInfo
 
         private void InitTypeTables()
         {
-            _typeNameTable = new string[3];
+            _typeNameTable = new string[4];
             _typeNameTable[0] = "CrashStats.MainPage";
             _typeNameTable[1] = "Windows.UI.Xaml.Controls.Page";
             _typeNameTable[2] = "Windows.UI.Xaml.Controls.UserControl";
+            _typeNameTable[3] = "CrashStats.VehicleResultPage";
 
-            _typeTable = new global::System.Type[3];
+            _typeTable = new global::System.Type[4];
             _typeTable[0] = typeof(global::CrashStats.MainPage);
             _typeTable[1] = typeof(global::Windows.UI.Xaml.Controls.Page);
             _typeTable[2] = typeof(global::Windows.UI.Xaml.Controls.UserControl);
+            _typeTable[3] = typeof(global::CrashStats.VehicleResultPage);
         }
 
         private int LookupTypeIndexByName(string typeName)
@@ -176,6 +178,7 @@ namespace CrashStats.CrashStats_XamlTypeInfo
         }
 
         private object Activate_0_MainPage() { return new global::CrashStats.MainPage(); }
+        private object Activate_3_VehicleResultPage() { return new global::CrashStats.VehicleResultPage(); }
 
         private global::Windows.UI.Xaml.Markup.IXamlType CreateXamlType(int typeIndex)
         {
@@ -200,6 +203,13 @@ namespace CrashStats.CrashStats_XamlTypeInfo
 
             case 2:   //  Windows.UI.Xaml.Controls.UserControl
                 xamlType = new global::CrashStats.CrashStats_XamlTypeInfo.XamlSystemBaseType(typeName, type);
+                break;
+
+            case 3:   //  CrashStats.VehicleResultPage
+                userType = new global::CrashStats.CrashStats_XamlTypeInfo.XamlUserType(this, typeName, type, GetXamlTypeByName("Windows.UI.Xaml.Controls.Page"));
+                userType.Activator = Activate_3_VehicleResultPage;
+                userType.SetIsLocalType();
+                xamlType = userType;
                 break;
             }
             return xamlType;
